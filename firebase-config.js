bf6b8951e1c350fa988087a6ca1dfd05c3af63c4
PJ Tech...... <xypher404x@gmail.com>
2026-09-13 23:20:31 +0300

    Add Firebase web configuration scaffold

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+/*
+ * FIREBASE CONFIGURATION
+ *
+ * Create a Firebase Web App in the Firebase Console and paste the
+ * configuration object below.
+ *
+ * The values in this file are normal client-side Firebase config values.
+ * Do NOT put private service-account keys here.
+ */
+
+window.FIREBASE_CONFIG = {
+    apiKey: "PASTE_API_KEY_HERE",
+    authDomain: "PASTE_PROJECT_ID_HERE.firebaseapp.com",
+    projectId: "PASTE_PROJECT_ID_HERE",
+    storageBucket: "PASTE_STORAGE_BUCKET_HERE",
+    messagingSenderId: "PASTE_MESSAGING_SENDER_ID_HERE",
+    appId: "PASTE_APP_ID_HERE",
+    measurementId: "PASTE_MEASUREMENT_ID_HERE"
+};
