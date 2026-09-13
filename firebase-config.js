5241921f8b14431d1f69b508c084db74bcd8f44e
PJ Tech...... <xypher404x@gmail.com>
2026-09-14 00:42:55 +0300

    Fix Firebase Web App configuration

--- firebase-config.js
+++ firebase-config.js
@@ -8,10 +8,10 @@
 
 window.FIREBASE_CONFIG = {
     apiKey: "AIzaSyDwh4-gC85lYUPjLz5B9-rOS2uGQx1CQ_4",
-    authDomain: "personalportfolioj.firebaseapp.com",
-    projectId: "personalportfolioj",
-    storageBucket: "personalportfolioj.firebasestorage.app",
-    messagingSenderId: "457231431344",
-    appId: "1:457231431344:web:520f06694c973c9283276",
-    measurementId: "G-HCS60NXBNB"
+    authDomain: "personalportfoliopj.firebaseapp.com",
+    projectId: "personalportfoliopj",
+    storageBucket: "personalportfoliopj.firebasestorage.app",
+    messagingSenderId: "457231431443",
+    appId: "1:457231431443:web:5206f06694c973c9283276",
+    measurementId: "G-HCS60NBXNB"
 };
